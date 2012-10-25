7c4dd82e7055921d3da59a381b1f9f0ff667a71f
Ben Couturier <ben.couturier@cern.ch>
2012-10-25 15:39:02 +0200

    Improved TimingAuditor

--- handlers/TimingHandler.py
+++ handlers/TimingHandler.py
@@ -21,44 +21,21 @@ class TimingHandler(BaseHandler):
     def collectResults(self,directory):
         #save current directory before chdir
         saved_previous_directory = os.getcwd()
-        try:
-            os.chdir(directory)
-            f = open('run.log')
-            lines = f.readlines()
-            f.close()
-        except OSError:
-            raise Exception(str(self.__class__)+": No result directory, check the given result directory")
-        except IOError:
-            raise Exception(str(self.__class__)+": Data file not found, this handler excepts a 'run.log' in the results directory' ")
 
-        for line in lines:
-            exp = re.compile('TimingAuditor\.T\.\.\.\s+INFO\s+EVENT LOOP\s*\|\s*[\d\.]+\s+\|\s*([\d\.]+)\s+\|.*$')
-            m = exp.search(line)
-            if not m is None:
-                ev = {}
-                tmp = m.groups()[0]
-                ev['EVENT_LOOP'] = tmp
-                colname = ""
-                value = ""
-        
-                for sl in lines:
-                    exp = re.compile('TimingAuditor\.T\.\.\.\s+INFO\s+(\w+)\s*\|\s*([\d\.]+)\s+\|\s*[\d\.]+\s+\|.*$')
-                    m = exp.search(sl)
-                    if not m is None:
-                        colname = m.groups()[0]
-                        value = m.groups()[1]
-                        self.add_column(colname)
-                        ev[colname] = value
-                        if colname == 'Output':
-                            self.results.append(ev)
-                            self.finished = True
-            
-            if self.finished:
-                break
-        
-        for e in self.results:
-            for col in self.column_names:
-                self.saveFloat(str(col), str(e[col]))
+        from timing.TimingParser import TimingParser
+        tp = TimingParser('run.log')
+
+        # Saving the top 10
+        for i, node in enumerate(tp.getTopN(10)):
+            self.saveString("top" + str(i), node.name.replace(" ", "_"))
+
+        # Now saving all the nodes
+        for node in tp.getAllSorted():
+            name = node.name.replace(" ", "_")
+            self.saveFloat(name, node.value)
+            self.saveFloat(name + "_count", node.entries)
 
         #go back to previous directory
         os.chdir(saved_previous_directory)
+
+
