5bdba72b81c3384f23bd53ed61693a6c94efee34
Marco Clemencic <marco.clemencic@cern.ch>
2012-12-05 17:50:49 +0100

    refactoring

--- LHCbNightly.cmake
+++ LHCbNightly.cmake
@@ -3,36 +3,24 @@ cmake_minimum_required(VERSION 2.8.5)
 set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
 include(lbnMacros)
 
-include(SlotConfig)
+load_config()
 
-set(sorted_projects)
-get_deps(sorted_projects ${projects})
-set(projects ${sorted_projects})
+#set(SLOT_BUILD_DIR "/tmp/marcocle/${slot}/build.${config}")
+set(SLOT_BUILD_DIR "${CMAKE_CURRENT_LIST_DIR}/build")
 
-set(SLOT_BUILD_DIR "/tmp/marcocle/${slot}/build.${config}")
+prepare_build_dir()
 
-# Checkout
-#execute_process(COMMAND checkoutSlot.py lhcb-gaudi-head)
-#execute_process(COMMAND git clone -b dev/cmake http://cern.ch/gaudi/Gaudi.git GAUDI/GAUDI_v23r5
-#                COMMAND getpack -Pr LHCb v35r3)
-execute_process(COMMAND rm -rf ${SLOT_BUILD_DIR})
-file(MAKE_DIRECTORY ${SLOT_BUILD_DIR})
-execute_process(COMMAND tar xf ${CMAKE_CURRENT_LIST_DIR}/${slot}.sources.tar.bz2
-                WORKING_DIRECTORY ${SLOT_BUILD_DIR})
-
-# Generate the project configuration valid now
-set(CTEST_PROJECT_NAME "${slot}")
-set(CTEST_BINARY_DIRECTORY "${SLOT_BUILD_DIR}")
 gen_projects_xml()
-#ctest_submit(FILES "${CTEST_BINARY_DIRECTORY}/Project.xml")
 
 # This choses which kind of build we are doing.
-set(Model Nightly)
+if(NOT DEFINED Model)
+  set(Model Nightly)
+endif()
 
 foreach(project ${projects})
   string(TOUPPER "${project}" PR)
-  if(project STREQUAL Gaudi)
-    set(SOURCE_DIR "${SLOT_BUILD_DIR}/GAUDI/GAUDI_v23r5")
+  if(DEFINED ${project}_dir)
+    set(SOURCE_DIR "${SLOT_BUILD_DIR}/${${project}_dir}")
   else()
     set(SOURCE_DIR "${SLOT_BUILD_DIR}/${PR}/${PR}_${${project}_version}")
   endif()
@@ -42,9 +30,6 @@ foreach(project ${projects})
 
   configure_file("${CMAKE_CURRENT_LIST_DIR}/CTestScript.template.cmake"
                  "${SOURCE_DIR}/CTestScript.cmake" @ONLY)
-
-  configure_file("${CMAKE_CURRENT_LIST_DIR}/CTestCustom.cmake"
-                 "${SOURCE_DIR}/CTestCustom.cmake" COPYONLY)
 
   configure_file("${CMAKE_CURRENT_LIST_DIR}/SlotConfig.cmake"
                  "${SOURCE_DIR}/SlotConfig.cmake" COPYONLY)
--- SlotConfig.cmake
+++ SlotConfig.cmake
@@ -3,6 +3,15 @@ set(slot lhcb-gaudi-head)
 set(config x86_64-slc6-gcc46-opt)
 
 set(projects Gaudi LHCb)
+
 set(Gaudi_version HEAD)
+# Override the default Gaudi source directory.
+set(Gaudi_dir GAUDI/GAUDI_v23r5)
+
 set(LHCb_version  v35r3)
 set(LHCb_dependencies Gaudi)
+
+set(CTEST_CUSTOM_WARNING_EXCEPTION
+    ${CTEST_CUSTOM_WARNING_EXCEPTION}
+    ".*/boost/.*"
+    "^--->> genreflex: WARNING:.*")
