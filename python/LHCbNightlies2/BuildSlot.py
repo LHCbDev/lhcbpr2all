8937e7c212925f1c66db5e5018703ff03f19a4ed
Marco Clemencic <marco.clemencic@gmail.com>
2012-12-08 19:05:39 +0100

    improvements: more options, better tarball name, refactoring

--- python/LHCbNightlies2/BuildSlot.py
+++ python/LHCbNightlies2/BuildSlot.py
@@ -11,6 +11,7 @@ import os
 from subprocess import Popen, call
 from string import Template
 from socket import gethostname
+from datetime import date
 
 log = logging.getLogger(__name__)
 
@@ -53,6 +54,14 @@ def parseConfigFile(path):
     import json
     return json.load(open(path))
 
+class ProjDesc():
+    def __init__(self, desc_dict):
+        self.name = desc_dict[u'name']
+        self.version = desc_dict[u'version']
+        self.deps = desc_dict.get(u'dependencies', [])
+        self.dir = os.path.join(self.name.upper(),
+                                '{0}_{1}'.format(self.name.upper(), self.version))
+
 def sortedByDeps(deps):
     '''
     Take a dictionary of dependencies as {'depender': ['dependee', ...]} and
@@ -63,12 +72,20 @@ def sortedByDeps(deps):
     ['0', '1', '3', '2', '4']
     '''
     def unique(l):
+        '''Return only the unique elements in the list l.
+
+        >>> unique([0, 0, 1, 2, 1])
+        [0, 1, 2]
+        '''
         u = []
         for i in l:
             if i not in u:
                 u.append(i)
         return u
     def recurse(keys):
+        '''
+        Recursive helper function to sort by dependency: for each key we
+        first add (recursively) its dependencies then the key itself.'''
         result = []
         for k in keys:
             result.extend(recurse(deps[k]))
@@ -98,7 +115,12 @@ def main():
                       metavar='SECONDS',
                       action='store', type='string',
                       help='set a global timeout on all tests (default: 600)')
-
+    parser.add_option('--build-only',
+                      action='store_true',
+                      help='build only, do not run the tests')
+    parser.add_option('--no-submit',
+                      action='store_true',
+                      help='do not submit the results to CDash server')
 
     parser.set_defaults(model=models[0],
                         level=logging.INFO,
@@ -113,6 +135,9 @@ def main():
 
     from os.path import join, dirname
     config = parseConfigFile(args[0])
+
+    # FIXME: we need something better
+    platform = os.environ['CMTCONFIG']
 
     from datetime import datetime
     starttime = datetime.now()
@@ -146,53 +171,53 @@ def main():
     ctestConfig = Template(open(join(dirname(__file__), 'CTestConfig.template.cmake')).read())
     ctestScript = Template(open(join(dirname(__file__), 'CTestScript.template.cmake')).read())
 
-    name2dir = {}
-    deps = {}
-    for p in config[u'projects']:
-        n = p[u'name']
-        v = p[u'version']
-        projdir = join(n.upper(), '{0}_{1}'.format(n.upper(), v))
-
-        # these are cached for later
-        name2dir[n] = projdir
-        deps[n] = p[u'dependencies']
-
-        projdir = join(build_dir, projdir)
+    projects = dict([(p.name, p) for p in map(ProjDesc, config[u'projects'])])
+    deps = dict([(p.name, p.deps) for p in projects])
+    timestamp = date.today().isoformat()
+    jobs = []
+    for p in sortedByDeps(deps):
+        p = projects[p]
+        projdir = join(build_dir, p.dir)
 
         shutil.copyfile(args[0], join(projdir, 'SlotConfig.json'))
         write(join(projdir, 'SlotConfig.cmake'), configCmake)
         write(join(projdir, 'CTestConfig.cmake'),
               ctestConfig.substitute(config))
         write(join(projdir, 'CTestScript.cmake'),
-              ctestScript.substitute({'project': n, 'version': v,
+              ctestScript.substitute({'project': p.name, 'version': p.version,
                                       'build_dir': build_dir, 'site': gethostname(),
                                       'Model': opts.model}))
 
-
-    jobs = []
-    for p in sortedByDeps(deps):
         build_cmd = ['ctest', '-DSTEP=BUILD', '--timeout', opts.timeout, '-S', 'CTestScript.cmake']
         test_cmd =  ['ctest', '-DSTEP=TEST', '--timeout', opts.timeout, '-S', 'CTestScript.cmake']
+
+        if opts.no_submit:
+            build_cmd.insert(1, '-DNO_SUBMIT=TRUE')
+            test_cmd.insert(1, '-DNO_SUBMIT=TRUE')
+
         if opts.level <= logging.INFO:
             build_cmd.insert(1, '-VV')
         if opts.level <= logging.DEBUG:
             test_cmd.insert(1, '-VV')
 
-        workdir = join(build_dir, name2dir[p])
+        log.info('building %s', p.dir)
+        call(build_cmd, cwd=projdir)
 
-        log.info('building %s', name2dir[p])
-        call(build_cmd, cwd=workdir)
+        log.info('packing %s', p.dir)
+        packname = [p.name, p.version,
+                    config[u'slot'], timestamp,
+                    platform, 'tar.bz2']
+        call(['tar', 'cjf', '.'.join(packname),
+              os.path.join(p.dir, 'InstallArea')], cwd=build_dir)
 
-        log.info('packing %s', name2dir[p])
-        call(['tar', 'cjf', os.path.basename(name2dir[p]) + '.tar.bz2',
-              os.path.join(name2dir[p], 'InstallArea')], cwd=build_dir)
+        if not opts.build_only:
+            log.info('testing (in background) %s', p.dir)
+            jobs.append(Popen(test_cmd, cwd=projdir))
 
-        log.info('testing (in background) %s', name2dir[p])
-        jobs.append(Popen(test_cmd, cwd=workdir))
-
-    log.info('waiting for tests still running...')
-    for j in jobs:
-        j.wait()
+    if jobs:
+        log.info('waiting for tests still running...')
+        for j in jobs:
+            j.wait()
 
     log.info('build completed in %s', datetime.now() - starttime)
     return 0
