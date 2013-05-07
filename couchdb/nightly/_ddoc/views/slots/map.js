acd8c393d1e14ebae5ab7d5399dbb854232129c5
Marco Clemencic <marco.clemencic@cern.ch>
2013-05-07 17:13:41 +0200

    improved to be closer to the JSON nightly conf

--- couchdb/nightly/_ddoc/views/slots/map.js
+++ couchdb/nightly/_ddoc/views/slots/map.js
@@ -1,8 +1,13 @@
 function(doc) {
   if (doc.type == "slot-config") {
+    var projs = [];
+    for(var idx in doc.projects) {
+      projs.push({name: doc.projects[idx].name,
+                  version: doc.projects[idx].version});
+    }
     emit([doc.date, doc.slot],
          {"build_id": doc.build_id,
           "platforms": doc.platforms,
-          "projects": doc.projects})
+          "projects": projs})
   }
 }
