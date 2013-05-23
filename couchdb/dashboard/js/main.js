fbb51368b88bb5aaa68864cc620181b9ad9970da
Marco Clemencic <marco.clemencic@cern.ch>
2013-05-23 09:29:30 +0200

    improved layout and added dynamic load of days
    Added buttons to trigger the load of a given day and the possibility to
    preselect the days to be shown (in a cookie).

--- couchdb/dashboard/js/main.js
+++ couchdb/dashboard/js/main.js
@@ -1,5 +1,11 @@
 var ARTIFACTS_BASE_URL = 'http://buildlhcb.cern.ch/artifacts/';
 
+// variable set from a cookie
+if (!$.cookie("enabled_days")) {
+	$.cookie("enabled_days", JSON.stringify(["Today"]));
+}
+var enabled_days = JSON.parse($.cookie("enabled_days"));
+
 function buildURL(slot, build_id, platform, project) {
 	return ARTIFACTS_BASE_URL + slot + '/' + build_id
 		+ '/summaries.' + platform + '/' + project + '/build_log.html';
@@ -102,33 +108,139 @@ jQuery.fn.lbSlotTable = function(data) {
 	return this;
 }
 
-// fill an element with one <div> per slot build for the given day
-jQuery.fn.lbNightly = function () {
-	return this.each(function(){
-		var el = $(this);
-		var day = moment(el.attr('day'));
-		var next = moment(day).add('days', 1);
-		var query = {'startkey': JSON.stringify([day.format('YYYY-MM-DD')]),
-					 'endkey': JSON.stringify([next.format('YYYY-MM-DD')])};
-		el.append($('<h1/>').append(day.format('dddd')).append('<hr/>'));
-		$.getJSON('_view/slots', query, function(data) {
-			$.each(data.rows, function(idx, row){
-				var slot = $('<div class="slot" slot="' + row.key[1]
-				             + '" build_id="' + row.value.build_id + '"/>');
-				slot.append($('<h4/>').append(row.key[1] + ': ' + row.value.description));
-				// FIXME: This is a bit convoluted, but it is needed to ensure that we work on the right element
-				$('.day[day="' + row.key[0] + '"]').append(slot);
-				slot.lbSlotTable(row);
-			});
+jQuery.fn.loadButton = function () {
+	return this.button({label: "show"})
+	.click(function () {
+		var day = moment($(this).attr('day'));
+		var next = moment(day).add('days', 1).format('YYYY-MM-DD');
+		day = day.format('YYYY-MM-DD');
+
+		$(this).unbind('click').button("disable");
+		$('img[day="' + day + '"]').show();
+		var jqXHR = $.ajax({dataType: "json",
+			url: '_view/slots',
+			data: {'startkey': JSON.stringify([day]),
+				'endkey': JSON.stringify([next])}});
+		jqXHR.day = day;
+		jqXHR.done(function(data, textStatus, jqXHR) {
+			var el = $('.day[day="' + jqXHR.day + '"] div.slots');
+			if (data.rows.length) {
+				$.each(data.rows, function(idx, row){
+					var slot = $('<div class="slot" slot="' + row.key[1]
+					+ '" build_id="' + row.value.build_id + '"/>');
+					slot.append($('<h4/>').append(row.key[1] + ': ' + row.value.description));
+					el.append(slot);
+					slot.lbSlotTable(row);
+				});
+			} else {
+				el.text('no data for this day');
+			}
+			el.show();
+			$('img[day="' + jqXHR.day + '"]').hide();
+			$('button[day="' + jqXHR.day + '"]').hideButton().button("enable");
 		});
 	});
 }
 
+jQuery.fn.hideButton = function () {
+	return this.button({label: "hide"})
+	.click(function() {
+		var day = $(this).attr("day");
+		$('.day[day="' + day + '"] div.slots').hide();
+		$(this).showButton();
+	});
+}
+
+jQuery.fn.showButton = function () {
+	return this.button({label: "show"})
+	.click(function() {
+		var day = $(this).attr("day");
+		$('.day[day="' + day + '"] div.slots').show();
+		$(this).hideButton();
+	});
+}
+
+// fill an element with one <div> per slot build for the given day
+jQuery.fn.lbNightly = function () {
+	return this.each(function(){
+		var day = moment($(this).attr('day'));
+		var btn = $('<button day="' + day.format('YYYY-MM-DD') + '">show</button>')
+		.loadButton();
+
+		$(this).append($('<div class="header"/>')
+				.append($('<span/>').append(btn))
+				.append('<span class="day-name"> ' + day.format('dddd') + ' </span>')
+				.append($('<img day="' + day.format('YYYY-MM-DD') + '" src="images/ajax-loader.gif" text="loading...">').hide())
+				)
+			.append($('<div class="slots"/>').hide());
+		if ($(this).hasClass("enabled"))
+			btn.click();
+	});
+}
+
 $(function(){
+	// prepare the dialog data
+	$('#filter-dialog input[name="enabled_days"]').val(enabled_days);
+	$('#all_days').button().click(function() {
+		$('#filter-dialog input:checkbox[name="enabled_days"]').prop("checked", true);
+	});
+	$('#no_days').button().click(function() {
+		$('#filter-dialog input:checkbox[name="enabled_days"]').prop("checked", false);
+	});
+	$("#filter-dialog").dialog({
+		autoOpen: false,
+		modal: true,
+		buttons: {
+	        OK: function() {
+	        	var values = [];
+	        	$('#filter-dialog input:checkbox[name="enabled_days"]:checked')
+	        	.each(function (idx, el) { values.push($(el).val()); });
+	        	enabled_days = values;
+	        	$.cookie("enabled_days", JSON.stringify(enabled_days));
+
+	        	var today = moment().format('dddd');
+        		var yesterday = moment().subtract('days', 1).format('dddd');
+
+        		$('div.day div.header button').each(function(){
+        			var btn = $(this);
+        			var day = moment(btn.attr('day')).format('dddd');
+        			if ($.inArray(day, enabled_days) >= 0
+        					|| (day == today && $.inArray('Today', enabled_days) >= 0)
+        					|| (day == yesterday && $.inArray('Yesterday', enabled_days) >= 0)) {
+        				if (btn.button('option', 'label') == 'show')
+        					btn.click();
+        			} else {
+        				if (btn.button('option', 'label') == 'hide')
+        					btn.click()
+        			}
+        		});
+	        	$(this).dialog("close");
+	        },
+	        Cancel: function() {
+	        	$(this).dialog("close");
+	        }
+		}
+	});
+
+	$("#set-filter")
+    	.button()
+    	.click(function() {
+    		$("#filter-dialog").dialog("open");
+    });
+
+	// Prepare day tables
 	var today = moment();
 	for(var day = 0; day < 7; day++) {
 		var d = moment(today).subtract('days', day);
-		$('#middle').append('<div class="day" day="' + d.format('YYYY-MM-DD') + '"/>');
+		var e = "disabled";
+		if (day == 0 && $.inArray('Today', enabled_days) >= 0) {
+			e = "enabled";
+		} else if (day == 1 && $.inArray('Yesterday', enabled_days) >= 0) {
+			e = "enabled";
+		} else if ($.inArray(d.format('dddd'), enabled_days) >= 0) {
+			e = "enabled";
+		}
+		$('#middle').append('<div class="day ' + e + '" day="' + d.format('YYYY-MM-DD') + '"/>');
 	}
 
 	$('.day').lbNightly();
