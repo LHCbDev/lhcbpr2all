1f922e793d8463096a5b4ada92fedf09cafe2330
Marco Clemencic <marco.clemencic@cern.ch>
2013-06-18 17:45:39 +0200

    removed obsolete ResultsDB module and added tests for Dashboard

--- python/LbNightlyTools/tests/test_db.py
+++ python/LbNightlyTools/tests/test_db.py
@@ -12,9 +12,47 @@
 # Uncomment to disable the tests.
 #__test__ = False
 
-from .. import ResultsDB
+import os
+import shutil
+import json
+from tempfile import mkdtemp
 
-DB_URL = 'http://localhost:5984/nightly_test_db'
+from LbNightlyTools.Utils import Dashboard
+Dashboard.COUCHDB_SERVER = 'http://dummyname:9999'
 
-def test_connection():
-    db = ResultsDB.connect(DB_URL)
+def test_instantiation():
+    tmpdir = mkdtemp()
+    try:
+        dash = Dashboard(submit=False)
+        assert dash.db is None
+        assert dash.dumpdir is None
+
+        # these must not fail, but need a real DB to do something useful
+        Dashboard()
+        Dashboard(credentials=('user', 'password'))
+
+    finally:
+        shutil.rmtree(tmpdir, ignore_errors=True)
+
+def test_filedump():
+    tmpdir = mkdtemp()
+    try:
+        dumpdir = os.path.join(tmpdir, 'db')
+        dash = Dashboard(dumpdir=dumpdir)
+        assert dash.dumpdir is dumpdir
+        assert os.path.isdir(dumpdir)
+
+        data = {'type': 'data-type',
+                'slot': 'my-slot',
+                'build_id': 121,
+                'project': 'MyProject'}
+        dash.publish(data)
+
+        filename = os.path.join(dumpdir, 'my-slot.121.MyProject.data-type.json')
+        assert os.path.isfile(filename)
+
+        dump = json.load(open(filename))
+        assert data == dump
+
+    finally:
+        shutil.rmtree(tmpdir, ignore_errors=True)
