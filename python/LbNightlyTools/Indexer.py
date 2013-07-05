c392be8e67ee0891bcaccb3752a96e36f8b09bac
Marco Clemencic <marco.clemencic@cern.ch>
2013-07-05 18:50:52 +0200

    fix to the relative path used in the glimpse index

--- python/LbNightlyTools/Indexer.py
+++ python/LbNightlyTools/Indexer.py
@@ -205,11 +205,11 @@ class Script(LbUtils.Script.PlainScript):
             ensureDirs([index_dir])
 
             glimpseindex = Popen(['glimpseindex', '-H', index_dir, '-F'],
-                                  cwd=proj_root,
+                                  cwd=build_dir,
                                   stdin=PIPE,
                                   stdout=glimpse_stdout)
             for f in filesToIndex(proj_root):
-                glimpseindex.stdin.write(f + '\n')
+                glimpseindex.stdin.write(join(proj.dir, f) + '\n')
             glimpseindex.stdin.close()
             glimpseindex.wait()
 
