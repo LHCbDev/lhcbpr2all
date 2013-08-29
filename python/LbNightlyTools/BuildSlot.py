LHCbDev/lhcbpr2all

commit 12026daf02e0a3b7cd63104f4cc68e29c8c45484

use less strict UTF decoding when reading build.log
Already partially introduced in commit 5dfe9fd, the fix was not applies
in all the required places.

python/LbNightlyTools/BuildSlot.py
@@ -1111,6 +1111,8 @@ class BuildReporter(object):
         full_log = reportFileName('.log')
         shutil.copy(self.build_log, full_log)
 
+        logfile = codecs.open(self.build_log, 'r', 'utf-8', errors='replace')
+
         # generate the small summary file with the counts of warnings
         f = open(log_summary, 'w')
         f.write(self._oldSummary())
@@ -1131,7 +1133,7 @@ class BuildReporter(object):
         env_block_size = len(env_lines)
         f.writelines(checkout_lines)
         checkout_block_size = 1
-        f.writelines(codecs.open(self.build_log, 'r', 'utf-8'))
+        f.writelines(logfile)
         f.close()
 
         # generate HTML summary main page
@@ -1154,7 +1156,6 @@ class BuildReporter(object):
             sections.append([name, begin, 0])
         if sections:
             sections[-1][-1] = self.summary['size']
-        logfile = codecs.open(self.build_log, 'r', 'utf-8')
         offset = 0
         for chunkname, lines in zip(['env'], [env_lines]):
             chunkname = join(chunksdir, chunkname)

commit b95c9735a4a6fd4a2c71a42c6e7108ef6d89c4f3

fixed the regression introduced with commit 12026da

python/LbNightlyTools/BuildSlot.py
@@ -1111,8 +1111,6 @@ class BuildReporter(object):
         full_log = reportFileName('.log')
         shutil.copy(self.build_log, full_log)
 
-        logfile = codecs.open(self.build_log, 'r', 'utf-8', errors='replace')
-
         # generate the small summary file with the counts of warnings
         f = open(log_summary, 'w')
         f.write(self._oldSummary())
@@ -1133,7 +1131,8 @@ class BuildReporter(object):
         env_block_size = len(env_lines)
         f.writelines(checkout_lines)
         checkout_block_size = 1
-        f.writelines(logfile)
+        f.writelines(codecs.open(self.build_log, 'r', 'utf-8',
+                                 errors='replace'))
         f.close()
 
         # generate HTML summary main page
@@ -1156,6 +1155,7 @@ class BuildReporter(object):
             sections.append([name, begin, 0])
         if sections:
             sections[-1][-1] = self.summary['size']
+        logfile = codecs.open(self.build_log, 'r', 'utf-8', errors='replace')
         offset = 0
         for chunkname, lines in zip(['env'], [env_lines]):
             chunkname = join(chunksdir, chunkname)