b7186c5c83a6fa5ba33ce0dad8d02e0b1c1e1934
Sofia Qirjazi <sqirjazi@pclhcb13.cern.ch>
2014-06-23 12:58:10 +0200

    fixed LBCORE-516: Added 'release' CouchApp.
    The release directory contains the files for the couchapp
    
    The application was developed based on the dashboard couchapp.
    
    Feature 1: The Rebuild button, allows the user to send a request
    	   to JENKINS and rebuild the slot with the given projects
    	   and platforms.
    Feature 2: After the rebuild is clicked, the confirmation dialog
    	   appears with related information and the user can either
    	   process or cancel.
    Feature 3: The slots are not separated by date, they are grouped
    	   to show only tha last weeks release-builds.
    Feature 4: The applying filtering rules can only be projects.
    Feature 5: Each slot has a link(direct link) to a url query that
    	   represents only that slot.
    Feature 6: When in queried page, the user has the "View All" option
    	   to make all the slots appear in the page.
    
    https://its.cern.ch/jira/browse/LBCORE-516

---
+++ couchdb/release/_ddoc/views/summariesByDate/map.js
@@ -0,0 +1,27 @@
+function(doc) {
+
+  if(doc.type == 'build-result' || doc.type == 'tests-result') {
+
+	var key = doc.completed;
+    var dateTemp = new Date(eval(key.substring(0,4)),eval(key.substring(5,7))-1,eval(key.substring(8,10)),eval(key.substring(11,13)),eval(key.substring(14,16)),eval(key.substring(17,19)));
+
+    var data = {slot: doc.slot, project: doc.project, platform: doc.platform,date:doc.completed,build_id:doc.build_id,date:dateTemp.toGMTString()};
+
+
+
+
+   if(doc.type == 'build-result'){
+	   data.build = {warnings: doc.warnings, errors: doc.errors};
+   }else{
+		data.tests = {failed: 0, total: doc.results.length};
+	    for (var idx in doc.results) {
+		if (doc.results[idx].outcome != 'PASS' &&
+		    doc.results[idx].outcome != 'UNTESTED' &&
+		    doc.results[idx].outcome != 'SKIPPED')
+		 data.tests.failed++;
+	    }
+   }
+   emit(key, data);
+
+  }
+}
