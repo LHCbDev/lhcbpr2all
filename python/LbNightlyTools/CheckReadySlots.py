f1a6eedc899e17984549df04eed715086e93b775
cpomies <colas.pomies@cern.ch>
2015-04-13 14:07:23 +0200

    Add some comment

--- python/LbNightlyTools/CheckReadySlots.py
+++ python/LbNightlyTools/CheckReadySlots.py
@@ -59,30 +59,36 @@ def main(*argv):
 
     slots = Set()
 
+    #get all json files for slot configuration
     files = glob.glob('configs/lhcb-*.json')
 
 
     for file_name in files:
         with open(file_name) as data_file:
             data = json.load(data_file)
+            #check if slot is not disable
             if (not 'disabled' in data) or data['disabled'] == False:
+                #extract attribute slot if exist
                 if u'slot' in data:
                     slots.add(data['slot'])
+                # if not extract slot name from filename
                 else:
                     slots.add(splitext(basename(file_name))[0])
 
 
 
     xmlParse = parse('configs/configuration.xml')
+    #Extract all slots name from configuration who doesn't have attribute disabled to true
     slots = slots | Set(el.get('name') for el in xmlParse.findall("slot") if el not in xmlParse.findall("slot[@disabled='true']"))
     print '\n'.join(slots)
 
     ready = []
 
+    #Init parameters for each slot
     for slot in slots:
         ready.append(JobParams(slot=slot))
 
-
+    #Create a file that contain JobParams for each slot
     for i, test_params in enumerate(ready):
             open(output_file.format(i), 'w').write(str(test_params) + '\n')
             print output_file.format(i), 'written.'
