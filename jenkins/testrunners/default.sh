b506fe7d62412977ab121b53ff3388d6aabca4a1
Marco Clemencic <marco.clemencic@cern.ch>
2015-07-15 18:10:38 +0200

    updated a few Jenkins scripts

--- jenkins/testrunners/default.sh
+++ jenkins/testrunners/default.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 
-echo time lbn-build --verbose --jobs 8 --timeout 18000 --build-id "${slot}.${slot_build_id}" --artifacts-dir "${ARTIFACTS_DIR}" --tests-only --projects ${project} ${prepare_opt} ${submit_opt} ${rsync_opt} ${coverity_opt} ${config_file}
-time lbn-build --verbose --jobs 8 --timeout 18000 --build-id "${slot}.${slot_build_id}" --artifacts-dir "${ARTIFACTS_DIR}" --tests-only --projects ${project} ${prepare_opt} ${submit_opt} ${rsync_opt} ${coverity_opt} ${config_file}
+set -x
+time lbn-test --verbose \
+              --build-id "${slot}.${slot_build_id}" \
+              --artifacts-dir "${ARTIFACTS_DIR}" \
+              --projects ${project} \
+              ${prepare_opt} \
+              ${submit_opt} \
+              ${rsync_opt} \
+              ${coverity_opt} \
+              ${config_file}
